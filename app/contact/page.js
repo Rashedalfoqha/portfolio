2b6962eed038e158261bfc55817acf59682bdfd6
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-05-23 15:41:27 +0300

    ss

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,4 +1,33 @@
+"use client";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 export default function Page() {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const apply = () => {
+    const serviceId = "service_wpohsl5";
+    const templateId = "template_05hqb17";
+    const userId = "znH3jKlhlPReHMYZa";
+    const templateParams = {
+      to_email: "rashedmohammadalfoqha@gmail.com",
+      from_name: `${email}`,
+      subject: subject,
+      message: message
+    };
+
+    // Assuming userPersonal is defined somewhere
+    emailjs
+      .send(serviceId, templateId, templateParams, userId)
+      .then((response) => {
+        console.log("Email sent successfully:", response);
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+  };
+
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md" key="1">
@@ -8,7 +37,13 @@ export default function Page() {
         <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
           Hello, I&apos;m Rashed Mohammed. You can contact me via email.
         </p>
-        <form action="#" className="space-y-8">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            apply();
+          }}
+          className="space-y-8"
+        >
           <div>
             <label
               htmlFor="email"
@@ -19,6 +54,8 @@ export default function Page() {
             <input
               type="email"
               id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="name@gmail.com"
               required
@@ -34,6 +71,8 @@ export default function Page() {
             <input
               type="text"
               id="subject"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="Let us know how we can help you"
               required
@@ -49,13 +88,23 @@ export default function Page() {
             <textarea
               id="message"
               rows="6"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Leave a comment..."
+              required
             ></textarea>
           </div>
           <button
             type="submit"
-            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            className="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 "
+            onClick={(e) => {
+              e.preventDefault();
+              apply();
+              setEmail("");
+              setMessage("");
+              setSubject("");
+            }}
           >
             Send message
           </button>
