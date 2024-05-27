30333a8bbc97eb72a4322ee087d2fe52b8581326
Rashedalfoqha <146785416+Rashedalfoqha@users.noreply.github.com>
2024-05-27 13:02:44 +0300

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,7 @@
-"use client";
+"use client"
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
+
 export default function Page() {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
@@ -15,9 +16,8 @@ export default function Page() {
       from_name: `${email}`,
       subject: subject,
       message: message
-    };
+    };  
 
-    // Assuming userPersonal is defined somewhere
     emailjs
       .send(serviceId, templateId, templateParams, userId)
       .then((response) => {
@@ -31,23 +31,26 @@ export default function Page() {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md" key="1">
-        <h2 className="mb-4 text-5xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+        <h2 className="mb-4 text-3xl lg:text-5xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
           Contact Us
         </h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-lg lg:text-xl">
           Hello, I&apos;m Rashed Mohammed. You can contact me via email.
         </p>
         <form
           onSubmit={(e) => {
             e.preventDefault();
             apply();
+            setEmail("");
+            setMessage("");
+            setSubject("");
           }}
-          className="space-y-8"
+          className="space-y-6 lg:space-y-8"
         >
           <div>
             <label
               htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Your email
             </label>
@@ -56,7 +59,7 @@ export default function Page() {
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="name@gmail.com"
               required
             />
@@ -64,7 +67,7 @@ export default function Page() {
           <div>
             <label
               htmlFor="subject"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Subject
             </label>
@@ -73,7 +76,7 @@ export default function Page() {
               id="subject"
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light p-2"
               placeholder="Let us know how we can help you"
               required
             />
@@ -81,7 +84,7 @@ export default function Page() {
           <div className="sm:col-span-2">
             <label
               htmlFor="message"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              className="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-400"
             >
               Your message
             </label>
@@ -90,21 +93,14 @@ export default function Page() {
               rows="6"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light p-2"
               placeholder="Leave a comment..."
               required
             ></textarea>
           </div>
           <button
             type="submit"
-            className="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 "
-            onClick={(e) => {
-              e.preventDefault();
-              apply();
-              setEmail("");
-              setMessage("");
-              setSubject("");
-            }}
+            className="py-3 px-6 text-sm font-medium text-center text-black rounded-lg bg-primary-700 sm:w-full hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
             Send message
           </button>
