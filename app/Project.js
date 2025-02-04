a616049bd75f50b1c9cdeae7493d035463a0020c
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2025-02-04 15:59:48 +0300

    view

--- app/Project.js
+++ app/Project.js
@@ -58,11 +58,6 @@ export default function Page() {
               <p className="text-gray-700 text-base">{project.des}</p>
             </div>
             <div className="px-6 pt-4 pb-2">
-              <a href={project.deployLink}>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:scale-110 transition duration-300">
-                  Deploy Link
-                </span>
-              </a>
               <a href={project.codeLink}>
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:scale-110 transition duration-300">
                   Code GitHub
